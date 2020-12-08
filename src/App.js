a1c1f8d7328f3df4c57c60a42c8e140d3a5bb117
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-08 22:12:22 +0900

    change readme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
     count: 0
   };
+
   add = () => {
     this.setState(current => ({ count: current.count + 1 }));
   };
+
   minus = () => {
     this.setState(current => ({ count: current.count + 1 }));
   };
+
+  componentDidMount() {
+    console.log("2.(componentDidMount)컴포넌트 렌더가 되었습니다 ");
+  }
+  componentDidUpdate() {
+    console.log('3.(componentDidUpdate)막 업데이트 되었습니다.')
+  }
+  componentWillUnmount() {
+    console.log('(componentWillUnmount)안녕 이제 끕니다~! ')
+  }
   render() {
+    console.log('1.(render)렌더 하는 중입니다.')
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
