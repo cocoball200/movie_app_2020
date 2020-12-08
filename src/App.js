f3e386c910227200729f818ee3c8c839aae703f4
Mihyeon Byeon <cocoball200@gmail.com>
2020-12-08 21:54:48 +0900

    setState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const singersList = [
-  { id: 1, name: 'Boa', img: 'https://res.heraldm.com/content/image/2018/01/26/20180126000818_0.jpg' },
-  { id: 2, name: 'Taemin', img: 'https://koreajoongangdaily.joins.com/data/photo/2020/09/07/2b2a0e5b-924a-4137-ae75-5d859a11f00a.jpg' },
-  { id: 3, name: 'BTS', img: 'https://blog.kakaocdn.net/dn/b71v0O/btqEw3k68Hw/5kmSi0lE9zzOFaO9WN7K41/img.jpg' },
-  { id: 4, name: 'KATTY PERRY', img: 'https://www.rollingstone.com/wp-content/uploads/2020/05/KatyPerry.jpg?resize=1800,1200&w=1800' },
-  { id: 5, name: 'SIA', img: 'https://blog.kakaocdn.net/dn/k09It/btqB9ymZNZR/6dXnwW6cksaanlw0ZvrAXK/img.jpg' }
-]
-
-function Singers({ name, img }) {
-  return (
-    <div>
-      <h1> 이번 페스티벌에 나올 사람은 {name}입니다</h1>
-      <img src={img} alt={name} />
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div>
-      {singersList.map(item => (
-        <Singers id={item.id} name={item.name} img={item.img} />
-      ))}
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
